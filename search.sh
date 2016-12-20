53b8b8cdf8d3532f296fc58c182afb2ed1f969cb
Anvitha <anvitha@anvitha-Vostro-260.(none)>
2016-12-20 15:55:58 +0530

    To delete the files that is 2 days older

---
+++ search.sh
@@ -0,0 +1,13 @@
+del_file()
+for entry in `ls $prga`
+do
+	echo $entry
+	var=$entry
+	find $var -mtime +2 -type f -delete
+done
+echo 'enter a value'
+read val
+if [ "$val" -ge "2" ]
+then
+	del_file
+fi
